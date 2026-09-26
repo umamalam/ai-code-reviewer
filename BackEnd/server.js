15ecb3a6230fb7646d4f1e83df4fb771335e44bf
UMAM <umamalam4@gmail.com>
2026-09-26 17:17:13 +0530

    Use PORT env var for hosting compatibility

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,6 @@ const app = require('./src/app')
 
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on http://localhost:3000')
 })// test webhook trigger
